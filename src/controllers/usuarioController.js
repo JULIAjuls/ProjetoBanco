821c7315ae50beeef5c246c550db2c9829300eab
Julia <mendoncajulia23@gmail.com>
2023-07-01 23:39:07 -0300

    finalizando a página de cadastro

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -1,8 +1,14 @@
-const Pessoa = require('../models/pessoa')
 const Usuario = require('../models/usuario')
 
 function loginView(req, res){
     res.render("login.html");
+}
+
+function login(req,res){
+    let usuario = {
+        email: req.body.email,
+        senha: req.body.senha
+    }
 }
 
 module.exports =  {
